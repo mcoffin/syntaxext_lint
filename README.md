5392def6e7d808f6403d5b04aa5b1f8c62c5b066
Matt Coffin <mcoffin13@gmail.com>
2015-06-02 14:09:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# syntaxext-lint
+
+This crate provides common lints for use with making syntax extensions.
+
+## Current Lints
+
+| Lint Name | Description |
+|-----------|-------------|
+| `dummy_span` | detects uses of `DUMMY_SP` |
---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+//! Provides some common lints for syntax extensions.
+#![feature(plugin_registrar)]
+#![feature(rustc_private)]
+
+extern crate syntax;
+#[macro_use]
+extern crate rustc;
+
+use syntax::ast;
+use syntax::parse::token;
+use rustc::lint::{Context, LintPass, LintPassObject, LintArray};
+use rustc::plugin;
+use std::ops::Deref;
+
+declare_lint!(DUMMY_SPAN,
+              Warn,
+              "detects uses of DUMMY_SP");
+
+struct Pass;
+
+impl Pass {
+    fn new() -> Pass {
+        Pass
+    }
+}
+
+impl LintPass for Pass {
+    fn get_lints(&self) -> LintArray {
+        lint_array!(DUMMY_SPAN)
+    }
+
+    fn check_expr(&mut self, cx: &Context, expr: &ast::Expr) {
+        match expr.node {
+            ast::ExprPath(None, ref path) => {
+                let name = token::get_ident(path.segments.last().unwrap().identifier);
+                if name.deref() == "DUMMY_SP" {
+                    cx.span_lint(DUMMY_SPAN, expr.span,
+                                 "usage of 'DUMMY_SP' is discouraged");
+                }
+            },
+            _ => {},
+        }
+    }
+}
+
+#[plugin_registrar]
+pub fn register_plugins(reg: &mut plugin::Registry) {
+    reg.register_lint_pass(Box::new(Pass::new()) as LintPassObject);
+}
